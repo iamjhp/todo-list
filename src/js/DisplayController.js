82f03b02652f348aa9326a25d4d15adf00a03b5f
iamjhp <jooho.park@outlook.com>
2022-03-22 12:14:40 +0100

    add pop-up for edit

--- src/js/DisplayController.js
+++ src/js/DisplayController.js
@@ -5,57 +5,138 @@ const content = document.querySelector(".content")
 const popUpForm = document.getElementById("form-popup")
 const addToDoListButton = document.querySelector(".add-task")
 const blocker = document.querySelector(".blocker")
-const addTaskButton = document.getElementById("addTask")
-const addTaskForm = document.querySelector(".form-container")
-const taskID = document.querySelectorAll(".todo-item[task-id]")
-const deleteButton = document.querySelector(".delete")
+const form = document.querySelector(".form-container")
+const form2 = document.querySelector(".form-container")
 
-addToDoListButton.addEventListener("click", openTaskFormPopUp)
-blocker.addEventListener("click", closeTaskFormPopUp)
+addToDoListButton.addEventListener("click", () => {
+    openTaskFormPopUp(false)
+    sumbitEventHandlerForNewTask()
+    blockerDivClickListener(handleTaskFromAddButtonEvent)
+})
 
-addTaskForm.addEventListener("submit", (e) => {
+const sumbitEventHandlerForNewTask = () => {
+    form.addEventListener("submit", handleTaskFromAddButtonEvent, 
+    {
+        once: true
+    })
+}
+
+const blockerDivClickListener = (callback) => {
+    blocker.addEventListener("click", () => {
+        form.removeEventListener("submit", callback)
+        togglePopUpHandler()
+    }, {
+        once: true
+    })
+}
+
+// handle after clicking the 'add task' button in the pop-up
+const handleTaskFromAddButtonEvent = (e) => {
     e.preventDefault()
     const {title, date, description, priority, project} = getNewTaskForm()
-
     const newTask = new Task(title, date, description, priority, project)
     ToDoList.saveNewTask(newTask)
+    form.removeEventListener("sumbit", handleTaskFromAddButtonEvent)
+    togglePopUpHandler()
+}
 
-    closeTaskFormPopUp()
-    addTaskForm.reset()
-})
+// handle after clicking the 'edit task' button in the pop-up
+const handleTaskFromEditButtonEvent = (e, item) => {
+    e.preventDefault()
+    const {title, date, description, priority, project} = getNewTaskForm()
+    item._name = title
+    item._date = date
+    ToDoList.editTask(item)
+    togglePopUpHandler()
+}
 
-/*
-deleteButton.forEach(item => {
-    item.addEventListener("click", () => 
-        console.log(item.getAttribute("task-id"))
-    )
-})
-*/
+const addDeleteEventListener = (itemID) => {
+    const deleteButton = document.querySelector(`.todo-item[task-id="${itemID}"]>.delete`)
+    deleteButton.addEventListener("click", () => {
+        const removedItem = document.querySelector(`.todo-item[task-id="${itemID}"]`);
+        removedItem.parentElement.removeChild(removedItem)
+        localStorage.removeItem(itemID)
+    })
+    blockerDivClickListener()
+}
 
-function togglePopUp() {
+const addEditEventListener = (itemID) => {
+    const editButton = document.querySelector(`.todo-item[task-id="${itemID._id}"]>.edit`)
+    editButton.addEventListener("click", () => {
+        form.removeEventListener("sumbit", handleTaskFromAddButtonEvent)
+        document.getElementById("title").value = itemID._name
+        document.getElementById("date").value = itemID._date
+        document.getElementById("description").value = itemID._description
+        document.getElementById("priority").value = itemID.priority
+        openTaskFormPopUp(true)
+        const handler = (e) => handleTaskFromEditButtonEvent(e, itemID)
+        submitEventHandlerForEditTask(handler)
+        blockerDivClickListener(handler)
+    })
+}
+
+const submitEventHandlerForEditTask = (callback) => {
+    form.addEventListener("submit", callback,
+    {
+        once: true
+    })
+}
+
+// open or close the form-popup
+function toggleFormWindow() {
     popUpForm.classList.toggle("open")
 }
 
-function openTaskFormPopUp() {
-    addTaskForm.reset()
-    togglePopUp()
-    toggleBlurBackground()
-}
-
-function closeTaskFormPopUp() {
-    togglePopUp()
-    toggleBlurBackground()
-}
-
+// set or remove the background blur
 function toggleBlurBackground() {
     content.classList.toggle("is-blurred")
 }
 
+function togglePopUpHandler() {
+    console.log("togglePopUpHandler")
+    toggleFormWindow()
+    toggleBlurBackground()
+}
+
+function openTaskFormPopUp(editBool = false) {
+    if (!editBool) form.reset() //clear form for add new task
+    addSubmitButtonForFrom(editBool)
+    togglePopUpHandler()
+}
+
+// returns form values
 function getNewTaskForm() {
     const title = document.getElementById("title").value
     const date = document.getElementById("date").value
     const description = document.getElementById("description").value
     const priority = document.getElementById("priority").value
-    const project = document.getElementById("project").value
-    return {title, date, description, priority, project}
+    return {title, date, description, priority}
 }
+
+// create the corresponding form pop-up content
+function addSubmitButtonForFrom(editBool = false) {
+    const formFooterDiv = document.querySelector(".form-footer")
+
+    let childNode = formFooterDiv.lastElementChild
+    while (childNode) {
+        formFooterDiv.removeChild(childNode)
+        childNode = formFooterDiv.lastElementChild
+    }
+   
+    if (editBool) {
+        const editButton = document.createElement("input")
+        editButton.setAttribute("type", "submit")
+        editButton.id = "editTask"
+        editButton.setAttribute("value", "edit task")
+        formFooterDiv.appendChild(editButton)
+    } else {
+        const sumbitButton = document.createElement("input")
+        form.reset()
+        sumbitButton.setAttribute("type", "submit")
+        sumbitButton.id = "addTask"
+        sumbitButton.setAttribute("value", "add task")
+        formFooterDiv.appendChild(sumbitButton)
+    }
+}
+
+export {addDeleteEventListener, addEditEventListener}
--- src/js/ToDoList.js
+++ src/js/ToDoList.js
@@ -1,14 +1,12 @@
 import format from 'date-fns'
 import Task from "./Task";
+import {addDeleteEventListener, addEditEventListener} from "./DisplayController"
 
 const todoListsClass = document.querySelector(".todo-lists")
 const taskID = document.querySelector(".task-id")
 const mainContainer = document.querySelector(".main-container")
 
 const todolist = (() => {
-
-    const getTaskID = () => {
-    }
 
     const displayTasks = () => {
         Object.keys(localStorage).forEach(key => {
@@ -17,6 +15,7 @@ const todolist = (() => {
             displayTask(item)
             
             addDeleteEventListener(item._id)
+            addEditEventListener(item)
         })
     }
 
@@ -24,22 +23,75 @@ const todolist = (() => {
         localStorage.setItem(item.id, JSON.stringify(item))
         displayTask(item)
         addDeleteEventListener(item._id)
+        addEditEventListener(item)
+    }
+
+    const editTask = (item) => {
+        const editedId = item._id
+        const editedTitle = item._name
+        const editedDate = item._date
+        const editedDespriction = item._decription
+        const editedPriority = item.priority
+        editHTMLSelektion(editedId, editedTitle, editedDate, editedDespriction, editedPriority)
+        addDeleteEventListener(item._id)
+        addEditEventListener(item)
+        editTaskInStorage(item)
+    }
+
+    const editTaskInStorage = (item) => {
+        localStorage.setItem(item._id, JSON.stringify(item))
+        
     }
 
     const displayTask = (item) => {
-        getHTMLSkeletonForTask(item._id, item._name, item._date)
+        getHTMLSkeletonForNewTask(item._id, item._name, item._date)
     }
 
-    const addDeleteEventListener = (itemID) => {
-        const deleteButton = document.querySelector(`.todo-item[task-id="${itemID}"]>.delete`)
-        deleteButton.addEventListener("click", () => {
-            const removedItem = document.querySelector(`.todo-item[task-id="${itemID}"]`);
-            removedItem.parentElement.removeChild(removedItem)
-            localStorage.removeItem(itemID)
-        })
+    const editHTMLSelektion = (id, name, date, description, priority) => {
+        const toDoItemDiv = document.querySelector(`.todo-item[task-id="${id}"]`)
+        let childNode = toDoItemDiv.lastElementChild
+        while(childNode) {
+            toDoItemDiv.removeChild(childNode)
+            childNode = toDoItemDiv.lastElementChild
+        }
+
+        const checkBoxLabel = document.createElement("label")
+        checkBoxLabel.classList.add("checkbox")
+        toDoItemDiv.appendChild(checkBoxLabel)
+        
+        const checkBoxInput = document.createElement("input")
+        checkBoxInput.type = "checkbox"
+        checkBoxLabel.appendChild(checkBoxInput)
+
+        const todoTitleSpan = document.createElement("span")
+        todoTitleSpan.classList.add("todo-title")
+        todoTitleSpan.appendChild(document.createTextNode(name))
+        toDoItemDiv.appendChild(todoTitleSpan)
+
+        const detailButton = document.createElement("button")
+        detailButton.classList.add("details")
+        detailButton.appendChild(document.createTextNode("DETAILS"))
+        toDoItemDiv.appendChild(detailButton)
+
+        const dateSpan = document.createElement("span")
+        dateSpan.classList.add("created-date")
+        dateSpan.appendChild(document.createTextNode(date))
+        toDoItemDiv.appendChild(dateSpan)
+
+        const editSpan = document.createElement("button")
+        editSpan.classList.add("edit")
+        editSpan.appendChild(document.createTextNode("edit"))
+        toDoItemDiv.appendChild(editSpan)
+
+        const deleteSpan = document.createElement("button")
+        deleteSpan.classList.add("delete")
+        deleteSpan.appendChild(document.createTextNode("delete"))
+        toDoItemDiv.appendChild(deleteSpan)
+
+        
     }
 
-    const getHTMLSkeletonForTask = (id, name, date) => {
+    const getHTMLSkeletonForNewTask = (id, name, date) => {
         const toDoItemDiv = document.createElement("div")
         toDoItemDiv.classList.add("todo-item")
         toDoItemDiv.setAttribute("task-id", id)
@@ -67,12 +119,12 @@ const todolist = (() => {
         dateSpan.appendChild(document.createTextNode(date))
         toDoItemDiv.appendChild(dateSpan)
 
-        const editSpan = document.createElement("span")
+        const editSpan = document.createElement("button")
         editSpan.classList.add("edit")
         editSpan.appendChild(document.createTextNode("edit"))
         toDoItemDiv.appendChild(editSpan)
 
-        const deleteSpan = document.createElement("span")
+        const deleteSpan = document.createElement("button")
         deleteSpan.classList.add("delete")
         deleteSpan.appendChild(document.createTextNode("delete"))
         toDoItemDiv.appendChild(deleteSpan)
@@ -80,7 +132,7 @@ const todolist = (() => {
         todoListsClass.appendChild(toDoItemDiv)
     }
 
-    return {displayTasks, saveNewTask}
+    return {displayTasks, saveNewTask, editTask}
 })()
 
 export default todolist
